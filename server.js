511a1534f4ff3e95227376886f3a89cb80cd9072
thle01 <lethehien18@gmail.com>
2020-03-15 23:33:36 -0700

    go

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const morgan = require('morgan');
+const hostname = 'localhost';
+const port = 3000;
+const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '\public'));
+app.use(bodyParser.json());
+app.use('/campsites',campsiteRouter);
+app.use('/promotions',promotionRouter);
+app.use('/partners',partnerRouter);
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
